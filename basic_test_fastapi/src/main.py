c64efcdc807f2e7d4ff96aefc93137f1d7932f04
Andrei DAMIAN <andrei@lummetry.ai>
2024-02-05 15:19:18 +0200

    prep for cluster

--- basic_test_fastapi/src/main.py
+++ basic_test_fastapi/src/main.py
@@ -5,9 +5,11 @@ from fastapi import FastAPI
 str_local_id = "test_" + str(uuid4())[:5]
 hostname = os.environ.get("HOSTNAME", "unknown")
 
+print("Initializing FastAPI app with ID: , HOSTNAME: {}...", str_local_id, hostname)
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
-  msg = f"Hello world from HOSTNAME: '{hostname}', ID: '{str_local_id}'"
+  msg = f"Hello world from HOSTNAME: '{hostname}', ID: '{str_local_id}'\n"
   return {"message": msg}
--- basic_test_fastapi/test.sh
+++ basic_test_fastapi/test.sh
@@ -1,3 +1,3 @@
-kubectl apply -f test_fastapi.yaml
+kubectl apply -f deploy.yaml
 kubectl get pods
-curl -L http://localhost:30081
+curl -L http://localhost:30050

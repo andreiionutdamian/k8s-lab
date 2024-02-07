79087531b86bdac89c13011f25c2284a5eb6a90b
Andrei Damian <andrei@lummetry.ai>
2024-02-07 10:01:49 +0000

    redis in basic py

---
+++ basic_test_fastapi/src/engine.py
@@ -0,0 +1,92 @@
+import os
+import redis
+import json
+from uuid import uuid4
+
+class AppPaths:
+  PATH_ROOT = "/"
+  PATH_STAT = "/stats"
+
+class AppHandler:
+  def __init__(self):
+    self.log = None
+    self.__setup()
+    return
+  
+  def P(self, s, **kwargs):
+    if vars(self).get('log') is not None :
+      self.log.P(s, **kwargs)
+    else:
+      print('[APH]' + s, flush=True, **kwargs)
+    return
+  
+  def __setup(self):
+    self.str_local_id = "test_" + str(uuid4())[:5]
+    self.__local_count = 0
+    self.__has_redis = False
+    self.hostname = os.environ.get("HOSTNAME", "unknown")    
+    self.P("Initializing App app with ID: {}, HOSTNAME: {}...".format(
+      self.str_local_id, self.hostname
+    ))
+    dct_env = dict(os.environ)
+    self.P("Environement:\n{}".format(json.dumps(dct_env, indent=2)))
+    self.__maybe_setup_redis()
+    return
+    
+  def __maybe_setup_redis(self):
+    self.__has_redis = False
+    return
+    
+  def _pack_result(self, message):
+    return {"result": message}
+  
+  def get_cluster_count(self):
+    result = self.__local_count
+    if self.__has_redis:
+      # get the value from redis
+      pass
+    return result
+  
+  
+  def handle_request(self, path):
+    self.__local_count += 1
+    if path == AppPaths.PATH_ROOT:
+      msg = self.handle_root()
+    elif path == AppPaths.PATH_STAT:
+      msg = self.handle_stat()
+    else:
+      msg = self.handle_generic(path)
+    result = self._pack_result(msg)
+    return result
+    
+
+  def handle_root(self):
+    msg = "Handler '{}', Local/Global: {}/{}, HOSTNAME: '{}', ID: '{}'".format(
+      AppPaths.PATH_ROOT, self.__local_count, self.get_cluster_count(),
+      self.hostname, self.str_local_id
+    )    
+    return msg
+  
+  def handle_stat(self):
+    dct_result = {
+      'info' : "Handler '{}', Worker HOSTNAME: '{}', ID: '{}'".format(
+        AppPaths.PATH_STAT, self.hostname, self.str_local_id
+      ),
+      'local_count' : self.__local_count,
+      'cluster_count' : self.get_cluster_count(),
+    }    
+    return dct_result
+  
+  
+  def handle_generic(self, path):
+    msg = "Generic handler '{}', Local/Global: {}/{}, HOSTNAME: '{}', ID: '{}'".format(
+      path, self.__local_count, self.get_cluster_count(),
+      self.hostname, self.str_local_id
+    )  
+    return msg
+
+
+if __name__ == '__main__':
+  eng = AppHandler()
+  print(eng.handle_request(AppPaths.PATH_ROOT))
+  print(eng.handle_request(AppPaths.PATH_STAT))
--- basic_test_fastapi/src/main.py
+++ basic_test_fastapi/src/main.py
@@ -1,27 +1,27 @@
 import os
 from uuid import uuid4
 from fastapi import FastAPI, APIRouter
+from .engine import AppHandler, AppPaths
 
-str_local_id = "test_" + str(uuid4())[:5]
-hostname = os.environ.get("HOSTNAME", "unknown")
 
-print("Initializing FastAPI app with ID: , HOSTNAME: {}...", str_local_id, hostname)
-
+engine = AppHandler()
 app = FastAPI()
 router1 = APIRouter()
-router2 = APIRouter()
 
-PATH_1 = "/"
-@router1.get(PATH_1)
+@router1.get(AppPaths.PATH_ROOT)
 async def root():
-  msg = f"Handler '{PATH_1}', Worker HOSTNAME: '{hostname}', ID: '{str_local_id}'\n"
-  return {"message": msg}
+  result = engine.handle(AppPaths.PATH_ROOT)
+  return result
+
+@router1.get(AppPaths.PATH_STAT)
+async def stat():
+  result = engine.handle(AppPaths.PATH_STAT)
+  return result
 
 # note: this is a catch-all route, so it should be the last route in the router
 @router1.get("/{full_path:path}", include_in_schema=False)
 async def catch_all(full_path: str):
-  msg = f"Generic handler on path:{full_path}, Worker HOSTNAME: '{hostname}', ID: '{str_local_id}'\n"
-  return {"message": msg}
+  result = engine.handle_request(full_path)
 
 
 app.include_router(router1)

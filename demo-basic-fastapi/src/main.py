c2eb01cfdb4a915b0e16c173d5ef49f7c0421592
Andrei Damian <andrei@lummetry.ai>
2024-02-08 06:45:55 +0000

    fix mandatory param

--- demo-basic-fastapi/src/main.py
+++ demo-basic-fastapi/src/main.py
@@ -10,18 +10,22 @@ router1 = APIRouter()
 
 ROUTE1 = AppPaths.PATH_ROOT['PATH']
 @router1.get(ROUTE1)
-async def root(parameter: str = Query(...)):
-  return engine.handle_request(ROUTE1, parameter=parameter)
+# async def root(data: str = Query(...)): # this makes the `data` parameter mandatory
+async def root(data: str = None):
+  print("Received request for root with params: ", data)
+  return engine.handle_request(ROUTE1, parameter=data)
 
 ROUTE2 = AppPaths.PATH_STAT['PATH']
 @router1.get(ROUTE2)
-async def stat(parameter: str = Query(...)):
-  return engine.handle_request(ROUTE2, parameter=parameter)
+async def stat(data: str = None):
+  print("Received request for stat with params: ", data)
+  return engine.handle_request(ROUTE2, parameter=data)
 
 # note: this is a catch-all route, so it should be the last route in the router
 @router1.get("/{full_path:path}", include_in_schema=False)
-async def catch_all(full_path: str, parameter: str = Query(...)):
-  return engine.handle_request(full_path, parameter=parameter)
+async def catch_all(full_path: str, data: str = None):
+  print("Received request for catch-all with params: ", data)
+  return engine.handle_request(full_path, parameter=data)
   
 
 app.include_router(router1)

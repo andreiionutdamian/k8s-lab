b74c7ff6e4a343a4120829cff1abd009802d1688
Andrei Damian <andrei@lummetry.ai>
2024-02-09 09:56:12 +0000

    fix: constr

--- demo-basic-fastapi/deploy3_postgres/reset_nfs_storage.sh
+++ demo-basic-fastapi/deploy3_postgres/reset_nfs_storage.sh
@@ -15,12 +15,13 @@ log_with_color "Mounting $NFS_SERVER to $MOUNT_POINT" yellow
 sudo mount -t nfs "$NFS_SERVER" "$MOUNT_POINT" && log_with_color "  NFS mount successful." green || log_with_color "  Error: NFS mount failed." red
 
 log_with_color "Checking if $MOUNT_POINT has files..." yellow
-if [ -z "$(ls -A $MOUNT_POINT)" ]; then
+NR_FILES=$(sudo ls -A "$MOUNT_POINT" | wc -l)
+if [ "$NR_FILES" -eq 0 ]; then
     log_with_color "  $MOUNT_POINT is empty." green
 else
-    log_with_color "  $MOUNT_POINT is not empty." yellow
+    log_with_color "  $MOUNT_POINT is not empty and contains $NR_FILES files..." yellow
     log_with_color "  Deleting all files in $MOUNT_POINT..." yellow
-    sudo rm -rf --preserve-root "$MOUNT_POINT/*" && log_with_color "    All files in $MOUNT_POINT have been successfully deleted." green || log_with_color "    Error: Failed to delete files in $MOUNT_POINT." red
+    sudo find "$MOUNT_POINT" -mindepth 1 -delete
 fi
 
 log_with_color "Unmounting $MOUNT_POINT..." yellow

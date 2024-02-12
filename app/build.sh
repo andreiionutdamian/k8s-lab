4325c2c13986e7b3d7b08a729633dd055f1b4dbc
Andrei Damian <andrei@lummetry.ai>
2024-02-12 06:17:58 +0000

    fix img names

--- app/build.sh
+++ app/build.sh
@@ -2,7 +2,7 @@ docker build -t aidamian/basic_test_fastapi -f Dockerfile_basic .
 docker push aidamian/basic_test_fastapi
 
 docker build -t aidamian/basic_cluster_serving_test -f Dockerfile_serving .
-docker push aidamian/base_cluster_serving_test
+docker push aidamian/basic_cluster_serving_test
 
 docker build -t aidamian/basic_cluster_monitor_test -f Dockerfile_monitor .
-docker push aidamian/base_cluster_monitor_test
+docker push aidamian/basic_cluster_monitor_test

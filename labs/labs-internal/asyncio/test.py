1381dc1036569442e0ab8f3f0c89205e253506d1
Andrei Damian <andrei@lummetry.ai>
2024-02-13 10:13:33 +0200

    asyncio lab

--- labs/labs-internal/asyncio/test.py
+++ labs/labs-internal/asyncio/test.py
@@ -2,30 +2,42 @@ from time import sleep, time
 import asyncio
 
 async def f1():
-  print("f1-1-blocking")
+  print("calling f1-1-blocking")
   sleep(1) # blocking
-  print("f1-2-external-call")
+  print("calling f1-2-blocking")
+  sleep(1) # blocking
+  print("calling f1-external-call")
   await asyncio.sleep(5) # yilds control to another coroutine
-  print("f1-3-blocking")
+  print("calling f1-3-blocking")
+  sleep(1)
+  print("calling f1-4-blocking")
   sleep(1)
   return "f1-result"
   
   
 async def f2():
-  print("f2-1-blocking")
+  print("calling f2-1-blocking")
   sleep(1)
-  print("f2-2-external-call")
+  print("calling f2-2-blocking")
+  sleep(1)
+  print("calling f2-external-call")
   await asyncio.sleep(5) # yields control to another coroutine
-  print("f2-3-blocking")
+  print("calling f2-3-blocking")
+  sleep(1)
+  print("calling f2-4-blocking")
   sleep(1)
   return "f2-result"
   
 async def f3():
-  print("f3-1-blocking")
+  print("calling f3-1-blocking")
   sleep(1)
-  print("f3-2-external-call")
+  print("calling f3-2-blocking")
+  sleep(1)
+  print("calling f3-external-call")
   await asyncio.sleep(5) # yields control to another coroutine
-  print("f3-3-blocking")
+  print("calling f3-3-blocking")
+  sleep(1)
+  print("calling f3-4-blocking")
   sleep(1)
   return "f3-result"
   

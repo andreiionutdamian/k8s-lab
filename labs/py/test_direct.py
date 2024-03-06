b75e5f1343a752d5b1bb382bec97b0d02538392a
Andrei Damian <andrei.damian@hyperfy.tech>
2024-03-06 09:46:41 +0000

    test

--- labs/py/test_direct.py
+++ labs/py/test_direct.py
@@ -3,11 +3,19 @@ from k8s_monitor.src.kmonitor.base import KubeMonitor, safe_jsonify
 if __name__ == '__main__':
   km = KubeMonitor()
   
-  apps = ['emqx', 'nvidia', 'basic-test']
+  # apps = ['emqx', 'nvidia', 'basic-test']
   
-  result = km.check_pods_by_names(apps)  
-  print("Pods {}:\n{}".format(apps, safe_jsonify(result, indent=2)))
+  # result = km.check_pods_by_names(apps)  
+  # print("Pods {}:\n{}".format(apps, safe_jsonify(result, indent=2)))
   
-  nodes = km.get_nodes_metrics()
-  print("Nodes:\n{}".format(safe_jsonify(nodes, indent=2)))
+  # nodes = km.get_nodes_metrics()
+  # print("Nodes:\n{}".format(safe_jsonify(nodes, indent=2)))
+  summary = km.summary()
+  NP = 3
+  result = {
+    'namespaces': summary['namespaces'],
+    'nodes': summary['nodes'],
+    f'pods-first-{NP}': summary['pods'][:NP]
+  }
+  print("Summary:\n{}".format(safe_jsonify(result, indent=2)))
   

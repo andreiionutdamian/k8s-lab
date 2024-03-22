ff87a0a80d344400bfb21c1c8114554620e22133
Andrei Damian <andrei@lummetry.ai>
2024-03-22 10:42:25 +0000

    fix: update_cache

--- app/src/monitor/monitor_app.py
+++ app/src/monitor/monitor_app.py
@@ -63,8 +63,6 @@ class MonitorApp(
     result = self.redis_sethash("models", model_type, model_name)
     if result:
       self.P(f"Cache update: {model_type} - {model_name}")
-      info = model.config.id2label
-      self.P(f"Model config: {info}")
     return result
 
   def set_model(self, model_type: str, model_name: str, labels: dict):

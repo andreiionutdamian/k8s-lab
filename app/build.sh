a1de601e80369c22e01a60f54ea6f19252205227
ispirtraian <traian.ispir@yahoo.com>
2024-04-05 15:12:01 +0300

    fix: version tags

--- app/build.sh
+++ app/build.sh
@@ -1,11 +1,13 @@
 docker build -t aidamian/simple_test_fastapi -f Dockerfile_basic .
 docker push aidamian/simple_test_fastapi
 
-docker build -t aidamian/simple_serving_test_gpu -f Dockerfile_serving_gpu .
-docker push aidamian/simple_serving_test_gpu
+serving_ver=${yq -e ".application.serving.version" ./demo-ai-app/manifests/version.yaml}
+docker build -t aidamian/simple_serving_test_gpu:${serving_ver} -f Dockerfile_serving_gpu .
+docker push aidamian/simple_serving_test_gpu:${serving_ver}
 
-docker build -t aidamian/simple_serving_test_cpu -f Dockerfile_serving_cpu .
-docker push aidamian/simple_serving_test_cpu
+docker build -t aidamian/simple_serving_test_cpu:${serving_ver} -f Dockerfile_serving_cpu .
+docker push aidamian/simple_serving_test_cpu:${serving_ver}
 
-docker build -t aidamian/simple_cluster_monitor_test -f Dockerfile_monitor .
-docker push aidamian/simple_cluster_monitor_test
+monitor_ver=${yq -e ".application.monitor.version" ./demo-ai-app/manifests/version.yaml}
+docker build -t aidamian/simple_cluster_monitor_test:${monitor_ver} -f Dockerfile_monitor .
+docker push aidamian/simple_cluster_monitor_test:${monitor_ver}
